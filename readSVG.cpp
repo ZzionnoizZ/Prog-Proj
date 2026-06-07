0058cd4f025b5b9808bb87a247e5f7f0fa0dbbbb
OlgaZubenko <up202508214@g.uporto.pt>
2026-06-07 14:51:56 +0100

    read

--- readSVG.cpp
+++ readSVG.cpp
@@ -51,113 +51,113 @@ namespace svg
         }
         
         if (name == "ellipse"){
-                int cx = elemento->IntAttribute("cx");
-                int cy = elemento->IntAttribute("cy");
-                const Point center = {cx, cy};
-                int rx = elemento->IntAttribute("rx");
-                int ry = elemento->IntAttribute("ry");
-                const Point radius = {rx, ry};
-                const char* fill = elemento->Attribute("fill");
-                Color cor = parse_color(fill);
-                elem = (new Ellipse(cor,center,radius));
-            }
-            else if (name == "circle"){
-                int cx = elemento->IntAttribute("cx");
-                int cy = elemento->IntAttribute("cy");
-                const Point center = {cx, cy};
-                int r = elemento->IntAttribute("r");
-                const char* fill = elemento->Attribute("fill");
-                Color cor = parse_color(fill);
-                elem = (new Circle(cor,center,r));
-            }
-            else if (name == "polyline"){
-                std::istringstream pontos(elemento-> Attribute("points"));
-                string ponto, func, dados;
-                vector<Point> points;
-                while (pontos >> ponto){
-                    Point p =Recolher(ponto);  
-                    points.push_back(p);
-                }
-                const char* stroke = elemento->Attribute("stroke");
-                Color cor = parse_color(stroke);
-                elem = (new Polyline(points, cor)); 
-            }
-            else if (name == "line"){
-                int x1 = elemento->IntAttribute("x1");
-                int y1 = elemento->IntAttribute("y1");
-                int x2 = elemento->IntAttribute("x2");
-                int y2 = elemento->IntAttribute("y2");
-                string func, dados;
-                vector<Point> points;
-                const char* stroke = elemento->Attribute("stroke");
-                Color cor = parse_color(stroke);
-                elem = (new Line(x1, y1, x2, y2, cor));
-            }
-            else if (name == "polygon"){
-                std::istringstream pontos(elemento-> Attribute("points"));
-                string ponto;
-                vector<Point> points;
-                while (pontos >> ponto){
-                    points.push_back(Recolher(ponto));
-                }
-                const char* fill = elemento->Attribute("fill");
-                Color cor = parse_color(fill);
-                elem = (new Polygon(points, cor));
-            }
-            else if (name == "rect"){
-                int x = elemento->IntAttribute("x");
-                int y = elemento->IntAttribute("y");
-                int width = elemento->IntAttribute("width");
-                int height = elemento->IntAttribute("height");
-                const char* fill = elemento->Attribute("fill");
-                Color cor = parse_color(fill);
-                elem = (new Rect(x, y, width, height, cor));
-            }
-            else if (name == "g"){
-                vector<SVGElement*> childs;
-                for (XMLElement* child = elemento->FirstChildElement(); child != nullptr; child = child->NextSiblingElement()){
-                    SVGElement* child_obj = parse_element(child);
-                    if (child_obj) childs.push_back(child_obj);
-                }
-                elem = new Group(childs);
-            }
-            else if (name == "use"){
-                const char* href_attr = elemento->Attribute("href");
-                if (href_attr && href_attr[0] == '#'){
-                    string href = string(href_attr);
-                    string ref_id = href.substr(1, href.size()-1);
-                    if (element_map.find(ref_id) != element_map.end()) {
-                        elem = parse_element(element_map[ref_id]);
-                    }
-                }
-            }
-            if (elem!=nullptr){
-                const char* transform = elemento->Attribute("transform");
-                Point origin = {0, 0};
-                const char* transform_or = elemento->Attribute("transform-origin");
-                if (transform_or) {
-                    origin = Recolher(transform_or); 
-                }
-                if (transform){
-                    string nome = transform;
-                    int idx = nome.find('(');
-                    nome = nome.substr(0, idx);
-                    if (nome == "translate"){
-                        Point t = Recolher(dados(transform));
-                        elem->translate(t);
-                    }
-                    if (nome == "rotate"){
-                        int a = stoi(dados(transform));
-                        elem->rotate(origin, a);
-                    }
-                    else if (nome == "scale"){
-                        int a = stoi(dados(transform));
-                        elem->scale(origin, a);
-                    }
-                }
-            }
-            return elem;
+            int cx = elemento->IntAttribute("cx");
+            int cy = elemento->IntAttribute("cy");
+            const Point center = {cx, cy};
+            int rx = elemento->IntAttribute("rx");
+            int ry = elemento->IntAttribute("ry");
+            const Point radius = {rx, ry};
+            const char* fill = elemento->Attribute("fill");
+            Color cor = parse_color(fill);
+            elem = (new Ellipse(cor,center,radius));
         }
+        else if (name == "circle"){
+            int cx = elemento->IntAttribute("cx");
+            int cy = elemento->IntAttribute("cy");
+            const Point center = {cx, cy};
+            int r = elemento->IntAttribute("r");
+            const char* fill = elemento->Attribute("fill");
+            Color cor = parse_color(fill);
+            elem = (new Circle(cor,center,r));
+        }
+        else if (name == "polyline"){
+            std::istringstream pontos(elemento-> Attribute("points"));
+            string ponto, func, dados;
+            vector<Point> points;
+            while (pontos >> ponto){
+                Point p =Recolher(ponto);  
+                points.push_back(p);
+            }
+            const char* stroke = elemento->Attribute("stroke");
+            Color cor = parse_color(stroke);
+            elem = (new Polyline(points, cor)); 
+        }
+        else if (name == "line"){
+            int x1 = elemento->IntAttribute("x1");
+            int y1 = elemento->IntAttribute("y1");
+            int x2 = elemento->IntAttribute("x2");
+            int y2 = elemento->IntAttribute("y2");
+            string func, dados;
+            vector<Point> points;
+            const char* stroke = elemento->Attribute("stroke");
+            Color cor = parse_color(stroke);
+            elem = (new Line(x1, y1, x2, y2, cor));
+        }
+        else if (name == "polygon"){
+            std::istringstream pontos(elemento-> Attribute("points"));
+            string ponto;
+            vector<Point> points;
+            while (pontos >> ponto){
+                points.push_back(Recolher(ponto));
+            }
+            const char* fill = elemento->Attribute("fill");
+            Color cor = parse_color(fill);
+            elem = (new Polygon(points, cor));
+        }
+        else if (name == "rect"){
+            int x = elemento->IntAttribute("x");
+            int y = elemento->IntAttribute("y");
+            int width = elemento->IntAttribute("width");
+            int height = elemento->IntAttribute("height");
+            const char* fill = elemento->Attribute("fill");
+            Color cor = parse_color(fill);
+            elem = (new Rect(x, y, width, height, cor));
+        }
+        else if (name == "g"){
+            vector<SVGElement*> childs;
+            for (XMLElement* child = elemento->FirstChildElement(); child != nullptr; child = child->NextSiblingElement()){
+                SVGElement* child_obj = parse_element(child);
+                if (child_obj) childs.push_back(child_obj);
+            }
+            elem = new Group(childs);
+        }
+        else if (name == "use"){
+            const char* href_attr = elemento->Attribute("href");
+            if (href_attr && href_attr[0] == '#'){
+                string href = string(href_attr);
+                string ref_id = href.substr(1, href.size()-1);
+                if (element_map.find(ref_id) != element_map.end()) {
+                    elem = parse_element(element_map[ref_id]);
+                }
+            }
+        }
+        if (elem!=nullptr){
+            const char* transform = elemento->Attribute("transform");
+            Point origin = {0, 0};
+            const char* transform_or = elemento->Attribute("transform-origin");
+            if (transform_or) {
+                origin = Recolher(transform_or); 
+            }
+            if (transform){
+                string nome = transform;
+                int idx = nome.find('(');
+                nome = nome.substr(0, idx);
+                if (nome == "translate"){
+                    Point t = Recolher(dados(transform));
+                    elem->translate(t);
+                }
+                if (nome == "rotate"){
+                    int a = stoi(dados(transform));
+                    elem->rotate(origin, a);
+                }
+                else if (nome == "scale"){
+                    int a = stoi(dados(transform));
+                    elem->scale(origin, a);
+                }
+            }
+        }
+        return elem;
+    }
 }
 
 svg::Point Recolher(string s){
